75bd09234d8ba7eec53826d06dd9bcc86201a384
l19328 <47017295+l19328@users.noreply.github.com>
2019-02-02 10:47:08 +0800

    Create build.sh
    add build container script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# we need to have openr:test and fib:test first ,import by hand
+# and fibhandler in /bin  
+docker run -itd --name openr1 --sysctl net.ipv6.conf.all.disable_ipv6=0 ezware/openr bash
+docker run -itd --name openr2 --sysctl net.ipv6.conf.all.disable_ipv6=0 ezware/openr bash
+docker run -itd --name openr3 --sysctl net.ipv6.conf.all.disable_ipv6=0 ezware/openr bash
+
+docker network create --subnet 12.13.14.0/24  --gateway=12.13.14.1 Net1
+docker network create --subnet 12.13.15.0/24  --gateway=12.13.15.1 Net2
+docker network create --subnet 12.13.16.0/24  --gateway=12.13.16.1 Net3
+docker network create --subnet 12.13.17.0/24  --gateway=12.13.17.1 Net4
+
+docker network connect Net1 --ip 12.13.14.2 openr1
+docker network connect Net2 --ip 12.13.15.2 openr1
+docker network connect Net2 --ip 12.13.15.3 openr2
+docker network connect Net3 --ip 12.13.16.3 openr2
+docker network connect Net3 --ip 12.13.16.4 openr3
+docker network connect Net4 --ip 12.13.17.4 openr3
+
