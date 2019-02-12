a770850bfd3fc88a1ca30e5c3da8ed74a133d48f
l19328 <47017295+l19328@users.noreply.github.com>
2019-02-12 17:35:03 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@
 docker run -itd --name openr --sysctl net.ipv6.conf.all.disable_ipv6=0 ezware/openrbuild bash
 docker exec openr ls /home
 docker exec openr sh -c "mkdir /home/openr2 && cp /home/openr/build/build_openr.sh . && \
-cd /home/openr2 && git init && git clone github.com/facebook/openr && pwd && ls -l"
+cd /home/openr2 && git init && git pull github.com/facebook/openr && pwd && ls -l"
 #docker run -itd --name openr2 --sysctl net.ipv6.conf.all.disable_ipv6=0 ezware/openrbuild bash
 #docker run -itd --name openr3 --sysctl net.ipv6.conf.all.disable_ipv6=0 ezware/openrbuild bash
 
