86bd1bbb4f0a2c5294b16fb9a1128c992399ab95
l19328 <47017295+l19328@users.noreply.github.com>
2019-02-13 11:45:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ docker exec openr ls /home
 #cd /home/openr2 && git clone https://github.com/facebook/openr.git && pwd && \
 #ls -l && cd openr/build && cp /build_openr.sh . && ls -l && ./build_openr.sh"
 
-echo -e 'Y\n' | docker exec openr sh -c "apt remove python-pip"
+ docker exec openr sh -c "echo -e 'Y\n' | apt remove python-pip"
 docker exec openr sh -c "./home/openr/build/build_openr.sh"
 
 
