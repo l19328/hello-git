adb23d42da91ad3cacc41aee6a7f4e1facd4f62d
l19328 <47017295+l19328@users.noreply.github.com>
2019-02-13 14:16:45 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ docker exec openr ls /home
 #cd /home/openr2 && git clone https://github.com/facebook/openr.git && pwd && \
 #ls -l && cd openr/build && cp /build_openr.sh . && ls -l && ./build_openr.sh"
 
-docker exec openr sh -c "wget https://bootstrap.pypa.io/get-pip.py  --no-check-certificate && python get-pip.py"
+docker exec openr sh -c "apt-get install wget && wget https://bootstrap.pypa.io/get-pip.py  --no-check-certificate && python get-pip.py"
 docker exec openr sh -c "./home/openr/build/build_openr.sh"
 
 
